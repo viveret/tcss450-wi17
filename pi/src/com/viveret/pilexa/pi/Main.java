922f550e01a75ce1169a766d6c89fd41f86a1308
viveret <viveret.amant@gmail.com>
2017-01-26 19:02:50 -0800

    Wiped out pi project

--- pi/src/com/viveret/pilexa/pi/Main.java
+++
@@ -1,9 +0,0 @@
-package com.viveret.pilexa.pi;
-
-import com.viveret.pilexa.pi.defaultskills.RepeatBackToMeSkill;
-
-public class Main {
-    public static void main(String [] args) {
-        RepeatBackToMeSkill.main(args);
-    }
-}
