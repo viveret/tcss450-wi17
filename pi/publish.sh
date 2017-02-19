6b6fb16d125edaf137008b8d704698b829070659
viveret <viveret.amant@gmail.com>
2017-02-19 08:50:54 -0800

    Improved helper scripts

--- pi/publish.sh
+++ pi/publish.sh
@@ -1,61 +1,63 @@
 #!/bin/bash
-cmd="spawn /usr/bin/rsync -Pvaz --progress"
-endpoint="cssgate.insttech.washington.edu:~/public_html/pi"
-expect="/usr/bin/expect"
 
-read -p "Enter UW NetID: " username
-read -sp "Enter password: " password
+cmd="/usr/bin/rsync -Pvaztu --partial-dir=build/rsync-tmp "
+endpoint="build/dist"
+cssgate="cssgate.insttech.washington.edu:~"
+endpoint_cssgate="public_html/pi"
+
+mkdir -p ${endpoint}
 
 gradle build
 
 echo -e "\n### Publishing Java byte code...\n"
-${expect} << EOC
-set timeout -1
-${cmd} build/classes/main/ ${username}@${endpoint}/bin
-expect "password:"
-send "${password}\r"
-expect eof
-EOC
+${cmd} build/classes/main/* ${endpoint}/bin
 
 echo -e "\n### Publishing php code...\n"
 chmod -R 755 src/com/viveret/pilexa/pi/*
-${expect} << EOC
-set timeout -1
-${cmd} src/com/viveret/pilexa/pi/php/ ${username}@${endpoint}/
-expect "password:"
-send "${password}\r"
-expect eof
-EOC
+${cmd} src/com/viveret/pilexa/pi/php/ ${endpoint}/
 
 echo -e "\n### Publishing configuration files...\n"
 chmod -R 755 build/resources/main/*
-${expect} << EOC
-set timeout -1
-${cmd} build/resources/main/ ${username}@${endpoint}/
-expect "password:"
-send "${password}\r"
-expect eof
-EOC
+${cmd} build/resources/main/ ${endpoint}/
 
 echo -e "\n### Publishing helper scripts...\n"
-${expect} << EOC
-set timeout -1
-${cmd} $(echo *.sh) ${username}@${endpoint}/
-expect "password:"
-send "${password}\r"
-expect eof
-EOC
+${cmd} $(echo *.sh) ${endpoint}/
 
 echo -e "\n### Publishing libraries...\n"
 gradle copyRuntimeLibs
-${expect} << EOC
-set timeout -1
-${cmd} build/dep/ ${username}@${endpoint}/lib
-expect "password:"
-send "${password}\r"
-expect eof
-EOC
+${cmd} build/dep/ ${endpoint}/lib
+
+if [[ $* == *--zip* ]] ;
+then
+    echo -e "\n### Zipping distributables...\n"
+    mv ${endpoint} ${endpoint}-tmp
+    mkdir -p ${endpoint}
+    zip ${endpoint}/pi.zip ${endpoint}-tmp
+    tar -zcvf ${endpoint}/pi.tgz ${endpoint}-tmp
+fi
+
+if  [[ $* == *--cssgate* ]] ;
+then
+    read -p "Enter UW NetID: " username
+    # read -sp "Enter password: " password
+
+    # expect="/usr/bin/expect"
+    # expect_pre=""
+    # expect_post="expect \"password:\" \n send \"@password\r\" \n expect eof\n"
+
+    endpoint_local="${endpoint}"
+    endpoint="${username}@${cssgate}/${endpoint_cssgate}"
+    # expect=${expect/@password/$password}
+
+    #${expect} << EOC
+    #set timeout -1
+    #spawn
+    ${cmd} ${endpoint_local}/ ${endpoint}
+    #${expect_post}
+    #EOC
+fi
 
 echo -e "\n### Done."
 
 # rm $rsyncPassFile
+
--- pi/run-cssgate.sh
+++ pi/run-cssgate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nice -20 java -Xmx2648m -Xms1024m -cp bin:lib/*:. com.viret.pilexa.pi.Main
+nice -20 java -Xmx2648m -Xms1024m -cp "$(echo lib/*.jar | tr ' ' ':'):bin:." com.viveret.pilexa.pi.Main
